ffbc0b5fd8046386385a0151981035b2688df970
Adema Beketayeva <oriharadem@gmail.com>
2020-02-14 17:17:39 +0600

    first commit

---
+++ app/SurveyResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SurveyResponse extends Model
+{
+    protected $guarded = [];
+
+    public function surveys()
+    {
+        return $this->belongsTo(Survey::class);
+    }
+
+    //
+}
